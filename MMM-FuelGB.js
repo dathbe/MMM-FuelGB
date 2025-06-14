0769961748929535e27d98226defd15413846605
dathbe <github@beffa.us>
2025-06-14 11:00:53 -0700

    Update MMM-FuelGB.js

--- MMM-FuelGB.js
+++ MMM-FuelGB.js
@@ -1,49 +1,85 @@
-Module.register('MMM-SunMap', {
-  // Default module config.
+/* MagicMirror²
+ * Module: MMM-EarthquakeAlerts
+ *
+ * By dathbe
+ * MIT Licensed.
+ */
+
+Module.register('MMM-EarthquakeAlerts', {
+
+  // Default config.
   defaults: {
-    updateInterval: 15, // minutes
-    // fadeSpeed: 500,
-    height: 450,
+    locations: [
+      { latitude: 39.1, longitude: -94.6, name: 'nowhere specific' },
+    ],
+    updateInterval: 30 * 60, // 30 minutes
+    magnitude1: 2.5,
+    distance1: 10 * 1609,
+    magnitude2: 4.0,
+    distance2: 60 * 1609,
+    magnitude3: 5.5,
+    distance3: 300 * 1609,
+    magnitude4: 7.0,
+    animationSpeed: 2 * 1000, // 2 seconds
   },
 
-  // Define required styles.
-  getStyles: function () {
-    return ['MMM-SunMap.css']
+  // Define required scripts.
+  getStyles() {
+    return ['MMM-EarthquakeAlerts.css']
   },
 
-  // Start the module.
-  start: function () {
+  // Define start sequence.
+  start() {
     Log.info('Starting module: ' + this.name)
+
+    this.messageText = ''
+
+    this.sendSocketNotification('EARTHQUAKE_REQUEST', this.config)
+
+    this.scheduleUpdate()
   },
 
-  firstLoad: function () {
-    // Schedule the UI load based on normal interval
+  scheduleUpdate: function (delay) {
+    var nextLoad = this.config.updateInterval * 1000
+    if (typeof delay !== 'undefined' && delay >= 0) {
+      nextLoad = delay
+    }
+
     var self = this
     setInterval(function () {
-      self.updateDom()
-    }, this.config.updateInterval * 60 * 1000)
+      Log.debug('[MMM-EarthquakeAlerts] Sending notification now')
+      self.sendSocketNotification('EARTHQUAKE_REQUEST', self.config)
+    }, nextLoad)
   },
 
-  notificationReceived: function (notification) {
-    if (notification === 'DOM_OBJECTS_CREATED') {
-      this.firstLoad()
+  // dom generator.
+  getDom() {
+    const self = this
+    // create html
+    const wrapper = document.createElement('div')
+    wrapper.className = 'EarthquakeAlertsDiv'
+    wrapper.innerHTML = self.messageText
+    if (self.messageText == '') {
+      wrapper.style.display = 'none'
+    }
+    return wrapper
+  },
+
+  // Deal with received notification
+  socketNotificationReceived(notification, payload) {
+    if (notification === 'EARTHQUAKE_ALERT') {
+      Log.debug('[MMM-EarthquakeAlerts] Earthquake message received!')
+      if (payload.quakeMessages.length > 0) {
+        this.messageText = payload.quakeMessages.join('<br>')
+      }
+      else {
+        this.messageText = ''
+      }
+      this.updateDom(this.config.animationSpeed)
     }
   },
 
-  getDom() {
-    const wrapper = document.createElement('div')
-    wrapper.className = 'SunMapDiv'
-    var SunMapImage = new Image()
-    SunMapImage.height = this.config.height
-    var currTime = new Date()
-    SunMapImage.src = `https://www.timeanddate.com/scripts/sunmap.php?iso=${currTime.getFullYear()}${currTime.getMonth()}${currTime.getDate()}T${currTime.getHours()}${currTime.getMinutes()}&earth=1`
-    wrapper.appendChild(SunMapImage)
+  // NOW NEED TO HAVE IT REFRESH THE FEED PERIODICALLY
+  // Maybe set it so that it will display location 1 if loc1 hits any of the criteria, then 2, then 3 (with a boolean on whether any given thing hits?)
 
-    const textBox = document.createElement('div')
-    textBox.className = 'date-time'
-    textBox.innerHTML = currTime.toLocaleString()
-    wrapper.appendChild(textBox)
-
-    return wrapper
-  },
 })
